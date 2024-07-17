a6905b798566974c4c80b0801d01742325c479ae
SolomonRicky <yizhen.c02@gmail.com>
2024-07-18 00:58:24 +0800

    Using nandwrite instead of mtd write
    NAND-aware tools must be used in Zbtlink-Z800AX since it uses NAND.
    
    Reference: https://openwrt.org/docs/techref/flash#nand-specific_tools_for_reading_and_writing_to_raw_nand
    
    Signed-off-by: YiZhen Choo <yizhen.c02@gmail.com>

--- Z800AX-Revert.sh
+++ Z800AX-Revert.sh
@@ -1,27 +1,57 @@
 #!/bin/ash
 
+# Function to print colored text
+print_color() {
+    local color_code=$1
+    local text=$2
+    echo -e "\033[${color_code}m${text}\033[0m"
+}
+
+# Check the board model
+BOARD_NAME=$(cat /etc/board.json | jsonfilter -e '@["model"]["name"]')
+
+if [ "$BOARD_NAME" != "Zbtlink ZBT-Z800AX" ]; then
+    print_color "31" "This firmware is only suitable for Zbtlink ZBT-Z800AX."
+    exit 1
+fi
+
 # Define variables
-URL="https://firmware.download.solomonricky.eu.org/firmware/custom/02-Jan-24-QSDK-qualcommax-ipq807x-zbtlink_zbt-z800ax-rootfs.bin"
-FILE="/tmp/02-Jan-24-QSDK-qualcommax-ipq807x-zbtlink_zbt-z800ax-rootfs.bin"
-EXPECTED_SHA256="d16e25f5266be02fd5627814f84f210ce43161ba982985b751fff0743bca05d1"
+ROOTFS_URL="https://firmware.download.solomonricky.eu.org/firmware/custom/17-July-24-QSDK-qualcommax-ipq807x-zbtlink_zbt-z800ax-rootfs.dump"
+ROOTFS_1_URL="https://firmware.download.solomonricky.eu.org/firmware/custom/17-July-24-QSDK-qualcommax-ipq807x-zbtlink_zbt-z800ax-rootfs_1.dump"
+ROOTFS="/tmp/17-July-24-QSDK-qualcommax-ipq807x-zbtlink_zbt-z800ax-rootfs.dump"
+ROOTFS_1="/tmp/17-July-24-QSDK-qualcommax-ipq807x-zbtlink_zbt-z800ax-rootfs_1.dump"
+ROOTFS_EXPECTED_SHA256="c4cb5b528ce35ac4b63c1af8c73aff7f29eb78b8b0cb1bbd83e092ff414f2237"
+ROOTFS_1_EXPECTED_SHA256="d16e25f5266be02fd5627814f84f210ce43161ba982985b751fff0743bca05d1"
+ROOTFS_PARTITION=$(grep '"rootfs"' /proc/mtd | awk -F: '{print $1}')
+ROOTFS_1_PARTITION=$(grep '"rootfs_1"' /proc/mtd | awk -F: '{print $1}')
+
+# Install packages
+print_color "34" "Installing packages... Please wait..."
+opkg update
+opkg install nand-utils
 
 # Download the file
-echo "Downloading firmware... Please wait..."
-wget -O $FILE $URL
+print_color "34" "Downloading firmware... Please wait..."
+wget -O $ROOTFS $ROOTFS_URL
+wget -O $ROOTFS_1 $ROOTFS_1_URL
 
 # Calculate the SHA-256 checksum
-ACTUAL_SHA256=$(sha256sum $FILE | awk '{ print $1 }')
+ROOTFS_ACTUAL_SHA256=$(sha256sum $ROOTFS | awk '{ print $1 }')
+ROOTFS_1_ACTUAL_SHA256=$(sha256sum $ROOTFS_1 | awk '{ print $1 }')
 
 # Compare the checksum
-if [ "$ACTUAL_SHA256" == "$EXPECTED_SHA256" ]; then
-    echo "This firmware only suitable for ZBT Z800AX"
+if [ "$ROOTFS_ACTUAL_SHA256" == "$ROOTFS_EXPECTED_SHA256" ] && [ "$ROOTFS_1_ACTUAL_SHA256" == "$ROOTFS_1_EXPECTED_SHA256" ]; then
+    print_color "32" "This firmware is suitable for Zbtlink ZBT-Z800AX."
     # Prompt user for confirmation
-    read -p "Do you confirm you want to revert to stock firmware (Y/N)? " CONFIRM
+    read -p "$(print_color "33" "Do you confirm you want to revert to stock firmware (Y/N)? ")" CONFIRM
     if [ "$CONFIRM" == "Y" ] || [ "$CONFIRM" == "y" ]; then
-        mtd write $FILE rootfs_1 && mtd -r write $FILE rootfs
+        mtd erase rootfs_1 && nandwrite /dev/$ROOTFS_1_PARTITION $ROOTFS_1
+        mtd erase rootfs && nandwrite /dev/$ROOTFS_PARTITION $ROOTFS
+        print_color "32" "Firmware reverted successfully."
+        reboot
     else
-        echo "Operation cancelled by user."
+        print_color "31" "Operation cancelled by user."
     fi
 else
-    echo "SHA-256 checksum does not match. File may be corrupted."
+    print_color "31" "SHA-256 checksum does not match. File may be corrupted."
 fi
